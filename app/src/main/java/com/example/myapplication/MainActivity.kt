fa87cc73d5e5bc402b72d0604fee41a73af65bb1
ali-ben-hammad <ali.benhammad-etu@etu.univh2c.ma>
2024-03-15 23:06:43 +0000

    header

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,15 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,36 +41,49 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @Composable
 fun Greeting(name: String) {
     Text(text = "Helloo $name!")
 }
 
+
 @Composable
-fun App(){
+fun Header() {
+    Row(
+        modifier = Modifier.fillMaxWidth()
+            .fillMaxHeight(0.1f)
+            .background(Color.Yellow)
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = "Court counter")
+        Image(
+            modifier = Modifier.size(40.dp, 40.dp),
+            painter = painterResource(id = R.drawable.dots),
+            contentDescription = "Three dots"
+        )
+    }
+}
+
+@Composable
+fun App() {
     MyApplicationTheme {
-        // A surface container using the 'background' color from the theme
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            Column {
-                Row (
-                    modifier = Modifier.fillMaxSize()
-
-
-                ){
-                    Text(text = "   Court counter")
-                    // three dots image
-                    
-
-                }
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Header()
             }
         }
     }
-
 }
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
